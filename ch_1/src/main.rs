e1da3b585931c0f220eaca7ab673efde97fe1d81
John Davies <jd.other@hotmail.com>
2024-09-24 19:04:17 +0700

    checking two elements are not equal (derived from PartialEq)

--- ch_1/src/main.rs
+++ ch_1/src/main.rs
@@ -8,7 +8,11 @@ fn main() {
     
     let a_equals_b = element_a == element_b;
     let a_equals_a = element_a == element_a_clone;
+    let a_not_equals_b = element_a != element_b;
+    let a_not_equals_a = element_a != element_a_clone;
 
     println!("element_a == element_b: {}", a_equals_b);
     println!("element_a == element_a: {}", a_equals_a);
+    println!("element_a != element_b: {}", a_not_equals_b);
+    println!("element_a != element_a: {}", a_not_equals_a);
 }
