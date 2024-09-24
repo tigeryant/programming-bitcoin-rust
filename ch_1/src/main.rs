be778713175e6436a82ba8746f576147a63e2eb2
John Davies <jd.other@hotmail.com>
2024-09-24 18:59:10 +0700

    printing equality checks

--- ch_1/src/main.rs
+++ ch_1/src/main.rs
@@ -1,6 +1,14 @@
 use ch_1::*;
 
 fn main() {
-    let element = FieldElement::new(1, 97);
-    println!("element: {:?}", element);
+    let element_a = FieldElement::new(1, 97);
+    let element_a_clone = FieldElement::new(1, 97);
+    let element_b = FieldElement::new(29, 97);
+    println!("element_a: {:?}", element_a);
+    
+    let a_equals_b = element_a == element_b;
+    let a_equals_a = element_a == element_a_clone;
+
+    println!("element_a == element_b: {}", a_equals_b);
+    println!("element_a == element_a: {}", a_equals_a);
 }
