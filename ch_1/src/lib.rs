27f48e2ff7ed715020b1c3681fabe2039dcfe69b
John Davies <jd.other@hotmail.com>
2024-09-24 19:29:21 +0700

    implementing add and subtract for the FieldElement struct

--- ch_1/src/lib.rs
+++ ch_1/src/lib.rs
@@ -1,3 +1,6 @@
+use std::ops::Add;
+use std::ops::Sub;
+
 #[derive(Debug)]
 pub struct FieldElement {
     num: u32,
@@ -5,6 +8,7 @@ pub struct FieldElement {
 }
 
 impl FieldElement {
+    // do we need to change this to allow for negative numbers?
     pub fn new (num: u32, prime: u32) -> Self {
         if num >= prime {
             panic!("num not in range 0 to {}", prime - 1);
@@ -19,5 +23,35 @@ impl FieldElement {
 impl PartialEq for FieldElement {
     fn eq(&self, other: &Self) -> bool {
         self.num == other.num && self.prime == other.prime
+    }
+}
+
+impl Add for FieldElement {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        if self.prime != other.prime {
+            panic!("Cannot add two numbers in different fields")
+        }
+        let num = (self.num + other.num) % self.prime;
+        FieldElement {
+            num,
+            prime : self.prime
+        }
+    }
+}
+
+impl Sub for FieldElement {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        if self.prime != other.prime {
+            panic!("Cannot subtract two numbers in different fields")
+        }
+        let num = (self.num - other.num) % self.prime;
+        FieldElement {
+            num,
+            prime : self.prime
+        }
     }
 }
