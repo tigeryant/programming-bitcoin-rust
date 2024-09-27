51de7f4f31877658f0a1f0e1f92eb631388f3ba2
John Davies <jd.other@hotmail.com>
2024-09-27 13:21:06 +0700

    temporarily commenting out the Point struct and tests

--- ch_2_elliptic_curves/src/lib.rs
+++ ch_2_elliptic_curves/src/lib.rs
@@ -1,13 +1,27 @@
+use std::ops::Add;
+
 #[derive(Debug)]
 pub struct Point {
     a: i128,
     b: i128,
-    x: i128,
-    y: i128
+    x: Option<i128>,
+    y: Option<i128>
 }
 
+/*
 impl Point {
-    pub fn new (x: i128, y: i128, a: i128, b: i128) -> Self {
+    pub fn new (x: Option<i128>, y: Option<i128>, a: i128, b: i128) -> Self {
+        // ignore this if statement if x and y are None, when x and y are None this is the point at infinity
+        // if x == None && y == None {
+        //     return Point {
+        //         a,
+        //         b,
+        //         x: None,
+        //         y: None
+        //     };
+        
+        // TODO re-write this - use a match with a pattern to check if x and y are both None
+
         if y.pow(2) != x.pow(3) + a * x + b {
             panic!("({}, {}) is not on the curve", x, y)
         }
@@ -25,3 +39,16 @@ impl PartialEq for &Point {
         self.x == other.x && self.y == other.y && self.a == other.a && self.b == other.b
     }
 }
+
+// implement point addition (Add)
+impl Add for &Point {
+    type Output = Point;
+
+    fn add(self, other: Self) -> Point {
+        
+        Point {
+
+        }
+    }
+}
+ */
--- ch_2_elliptic_curves/tests/point.rs
+++ ch_2_elliptic_curves/tests/point.rs
@@ -1,9 +1,9 @@
+/*
 use ch_2_elliptic_curves::*;
 
 #[test]
 fn creates_valid_point() {
-    let point = Point::new(-1, -1, 5, 7);
-    assert!(&point == &point);
+    let _point = Point::new(-1, -1, 5, 7);
 }
 
 #[test]
@@ -11,3 +11,19 @@ fn creates_valid_point() {
 fn creates_invalid_point() {
     let _invalid_point = Point::new(-1, -2, 5, 7);
 }
+
+#[test]
+fn a_equals_b() {
+    let point_a = Point::new(-1, -1, 5, 7);
+    let point_b = Point::new(-1, -1, 5, 7);
+    assert!(&point_a == &point_b);
+}
+
+#[test]
+fn a_not_equals_b() {
+    let point_a = Point::new(-1, -1, 5, 7);
+    let point_b = Point::new(18, 77, 5, 7);
+    assert!(&point_a != &point_b);
+}
+
+*/
