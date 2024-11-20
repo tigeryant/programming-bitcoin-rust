eec3fd4420b5932b56784bf284f1bd1177aefe8f
John Davies <jd.other@hotmail.com>
2024-11-20 19:02:00 +0700

    Added script serialization methods

--- ecc/src/script.rs
+++ ecc/src/script.rs
@@ -59,4 +59,47 @@ impl Script {
         }
         Ok(Self { commands })
     }
+
+    fn raw_serialize(&self) -> Vec<u8> {
+        let mut result = vec![];
+        for cmd in &self.commands {
+            if cmd.len() == 1 { // must be an opcode
+                let op_code = cmd[0];
+                result.push(op_code);
+            } else {
+                let length = cmd.len();
+                if length < 76 { // encode length as a single byte
+                    result.push(length as u8);
+                } else if length <= 0xff { // op_pushdata1, then encode length as a byte
+                    result.push(76);
+                    result.push(length as u8);
+                } else if length <= 520 { // op_pushdata2, then encode length as two bytes
+                    result.push(77);
+                    result.extend_from_slice(&length.to_le_bytes()[..2]);
+                } else { // if it's longer than 520 bytes it's invalid - error
+                    panic!("too long a cmd");
+                }
+                result.extend_from_slice(cmd);
+            }
+        }
+        result
+    }
+
+    /// Serializes the script into a byte vector
+    pub fn serialize(&self) -> Vec<u8> {
+        let mut result = self.raw_serialize();
+        let total = result.len();
+        let mut length_bytes = vec![];
+        if total < 0xfd {
+            length_bytes.push(total as u8);
+        } else if total <= 0xffff {
+            length_bytes.push(0xfd);
+            length_bytes.extend_from_slice(&(total as u16).to_le_bytes());
+        } else {
+            length_bytes.push(0xfe);
+            length_bytes.extend_from_slice(&(total as u32).to_le_bytes());
+        }
+        length_bytes.append(&mut result);
+        length_bytes
+    }
 }
