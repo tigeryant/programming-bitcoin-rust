8cd0347bc985593f79868df0b748f1a40a6bc0b3
John Davies <jd.other@hotmail.com>
2024-12-12 23:47:39 +1300

    Added testnet parameter to to_address methods to allow for changing the network prefix

--- programming-bitcoin/src/address/address.rs
+++ programming-bitcoin/src/address/address.rs
@@ -1,14 +1,20 @@
 use crate::utils::base58::encode_base58_checksum;
 
-pub fn h160_to_p2sh_address(h160: &str) -> String {
-    let network_prefix: u8 = 0x05; // mainnet prefix for P2SH
+pub fn h160_to_p2sh_address(h160: &str, testnet: bool) -> String {
+    let network_prefix: u8 = match testnet {
+        true => 0xc4, // testnet prefix for P2SH
+        false => 0x05 // mainnet prefix for P2SH
+    };
     let mut h160_vec = hex::decode(h160).unwrap();
     h160_vec.insert(0, network_prefix);
     encode_base58_checksum(&h160_vec)
 }
 
-pub fn h160_to_p2pkh(h160: &str) -> String {
-    let network_prefix: u8 = 0x00; // mainnet prefix for P2PKH
+pub fn h160_to_p2pkh(h160: &str, testnet: bool) -> String {
+    let network_prefix: u8 = match testnet {
+        true => 0x6F, // testnet prefix for P2PKH
+        false => 0x05 // mainnet prefix for P2PKH
+    };
     let mut h160_vec = hex::decode(h160).unwrap();
     h160_vec.insert(0, network_prefix);
     encode_base58_checksum(&h160_vec)
--- programming-bitcoin/tests/address/address.rs
+++ programming-bitcoin/tests/address/address.rs
@@ -1,13 +1,13 @@
 #[test]
 fn test_h160_to_p2sh_address() {
-    let address = address::h160_to_p2sh_address("74d691da1574e6b3c192ecfb52cc8984ee7b6c56");
+    let address = address::h160_to_p2sh_address("74d691da1574e6b3c192ecfb52cc8984ee7b6c56", false);
     let expected = String::from("3CLoMMyuoDQTPRD3XYZtCvgvkadrAdvdXh");
     assert_eq!(expected, address);
 }
 
 #[test]
 fn test_h160_to_p2pkh_address() {
-    let address = address::h160_to_p2sh_address("74d691da1574e6b3c192ecfb52cc8984ee7b6c56");
+    let address = address::h160_to_p2pkh_address("74d691da1574e6b3c192ecfb52cc8984ee7b6c56", false);
     let expected = String::from("1Cdid9KFAaatwczBwBttQcwXYCpvK8h7FK");
     assert_eq!(expected, address);
 }
