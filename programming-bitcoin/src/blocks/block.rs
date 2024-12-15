2a23a1c3a601905ec89fc080f46a995ddc79c9ee
John Davies <jd.other@hotmail.com>
2024-12-16 12:02:16 +1300

    Implemented Display method for Block, and new_block test

--- programming-bitcoin/src/blocks/block.rs
+++ programming-bitcoin/src/blocks/block.rs
@@ -1,15 +1,17 @@
 use std::io::{Cursor, Read, Error};
+use std::fmt;
 
 pub struct Block {
-    version: [u8; 4], // little endian
-    prev_block: [u8; 32], // little endian
-    merkle_root: [u8; 32], // little endian
-    timestamp: [u8; 4], // little endian
-    bits: [u8; 4],
-    nonce: [u8; 4]
+    pub version: [u8; 4], // little endian
+    pub prev_block: [u8; 32], // little endian
+    pub merkle_root: [u8; 32], // little endian
+    pub timestamp: [u8; 4], // little endian
+    pub bits: [u8; 4],
+    pub nonce: [u8; 4]
 }
 
 impl Block {
+    // all fields should be given in little endian
     pub fn new(version: [u8; 4], prev_block: [u8; 32], merkle_root: [u8; 32], timestamp: [u8; 4], bits: [u8; 4], nonce: [u8; 4]) -> Self {
         Self {
             version,
@@ -43,5 +45,17 @@ impl Block {
             bits,
             nonce
         })
+    }
+}
+
+impl fmt::Display for Block {
+    // note that these fields are all displayed in big endian
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "Version: {}", hex::encode(self.version.iter().rev().cloned().collect::<Vec<u8>>()))?;
+        writeln!(f, "Previous Block: {}", hex::encode(self.prev_block.iter().rev().cloned().collect::<Vec<u8>>()))?;
+        writeln!(f, "Merkle Root: {}", hex::encode(self.merkle_root.iter().rev().cloned().collect::<Vec<u8>>()))?;
+        writeln!(f, "Timestamp: {}", hex::encode(self.timestamp.iter().rev().cloned().collect::<Vec<u8>>()))?;
+        writeln!(f, "Bits: {}", hex::encode(self.bits.iter().rev().cloned().collect::<Vec<u8>>()))?;
+        writeln!(f, "Nonce: {}", hex::encode(self.nonce.iter().rev().cloned().collect::<Vec<u8>>()))
     }
 }
--- programming-bitcoin/tests/blocks/mod.rs
+++ programming-bitcoin/tests/blocks/mod.rs
@@ -9,7 +9,20 @@ fn parse_block() {
     assert!(Block::parse(&mut stream).is_ok());
 }
 
-// #[test]
-// fn new_block() {
+#[test]
+fn new_block() {
+    let version: [u8; 4] = hex::decode("02000020").unwrap().try_into().unwrap();
+    let prev_block: [u8; 32] = hex::decode("8ec39428b17323fa0ddec8e887b4a7c53b8c0a0a220cfd000000000000000000").unwrap().try_into().unwrap();
+    let merkle_root: [u8; 32] = hex::decode("5b0750fce0a889502d40508d39576821155e9c9e3f5c3157f961db38fd8b25be").unwrap().try_into().unwrap();
+    let timestamp: [u8; 4] = hex::decode("02000020").unwrap().try_into().unwrap();
+    let bits: [u8; 4] = hex::decode("02000020").unwrap().try_into().unwrap();
+    let nonce: [u8; 4] = hex::decode("02000020").unwrap().try_into().unwrap();
+    let block = Block::new(version, prev_block, merkle_root, timestamp, bits, nonce);
 
-// }
+    assert_eq!(block.version, version);
+    assert_eq!(block.prev_block, prev_block);
+    assert_eq!(block.merkle_root, merkle_root);
+    assert_eq!(block.timestamp, timestamp);
+    assert_eq!(block.bits, bits);
+    assert_eq!(block.nonce, nonce);
+}
