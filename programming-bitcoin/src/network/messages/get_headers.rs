c2c540245784185a11a47b6b8f57ed27a8a6ced3
John Davies <jd.other@hotmail.com>
2024-12-27 14:05:24 +1300

    Implemented NetworkMessage trait methods for GetHeadersMessage (except for parse)

--- programming-bitcoin/src/network/messages/get_headers.rs
+++ programming-bitcoin/src/network/messages/get_headers.rs
@@ -1,9 +1,13 @@
-// use crate::network::network_message::NetworkMessage;
+use crate::{
+    network::{get_block_tip::get_block_tip, network_message::NetworkMessage},
+    utils::varint::encode_varint,
+};
+use std::io::{Cursor, Error};
 
 pub struct GetHeadersMessage {
     pub command: String,
     pub version: u32,
-    pub num_hashes: Vec<u8>,
+    pub num_hashes: u64,
     pub start_block: u32,
     pub end_block: u32,
 }
@@ -11,8 +15,6 @@ pub struct GetHeadersMessage {
 impl GetHeadersMessage {
     pub fn new(version: u32, num_hashes: u64, start_block: u32, end_block: Option<u32>) -> Self {
         let command = String::from("getheaders");
-
-        let num_hashes = num_hashes.to_le_bytes().to_vec();
 
         let end_block = end_block.unwrap_or(0);
 
@@ -26,12 +28,31 @@ impl GetHeadersMessage {
     }
 }
 
-/*
 impl NetworkMessage for GetHeadersMessage {
-    todo!();
-    // command
-    // serialize
-    // parse
-    // default_async
+    fn command(&self) -> &str {
+        &self.command
+    }
+
+    // Serializes an instance of self into a byte vector
+    fn serialize(&self) -> Vec<u8> {
+        let mut result = Vec::new();
+
+        result.extend_from_slice(&self.version.to_le_bytes());
+
+        result.extend_from_slice(&encode_varint(self.num_hashes));
+
+        result.extend_from_slice(&self.start_block.to_le_bytes());
+
+        result.extend_from_slice(&self.end_block.to_le_bytes());
+
+        result
+    }
+
+    fn parse(&self, _: &mut Cursor<Vec<u8>>) -> Result<Self, Error> {
+        todo!()
+    }
+
+    async fn default_async(_: &str) -> Result<Self, Error> {
+        Ok(Self::new(70015, 1, get_block_tip().await.unwrap(), None))
+    }
 }
-*/
