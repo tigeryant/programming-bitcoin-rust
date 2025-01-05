db77bf1048a9869c511a4a2f5ee181b9b4ea0624
John Davies <jd.other@hotmail.com>
2025-01-05 20:26:50 +1300

    Added validate_merkle_root method, split Block and BlockHeader in to separate structs

---
+++ programming-bitcoin/src/blocks/block.rs
@@ -0,0 +1,73 @@
+use std::io::{ Cursor, Error };
+
+use crate::{spv::utils::merkle_root, transactions::tx::Tx, utils::varint::{encode_varint, read_varint}};
+
+use super::block_header::BlockHeader;
+
+#[derive(Clone)]
+pub struct Block {
+    pub header: BlockHeader,
+    pub txs: Vec<Tx>
+}
+
+impl Block {
+    pub fn new(header: BlockHeader, txs: Vec<Tx>) -> Self {
+        Self {
+            header,
+            txs
+        }
+    }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        let mut result = Vec::new();
+
+        result.extend(self.header.serialize());
+
+        let tx_count: u64 = self.txs.len() as u64;
+
+        result.extend_from_slice(&encode_varint(tx_count));
+
+        for tx in &self.txs {
+            result.extend_from_slice(&tx.serialize());
+        }
+
+        result
+    }
+
+    pub fn parse(reader: &mut Cursor<Vec<u8>>) -> Result<Self, Error> {
+        let header = BlockHeader::parse(reader)?;
+
+        let tx_count = read_varint(reader)?;
+
+        // testnet true for now
+        let testnet = true;
+
+        let txs = (0..tx_count)
+            .map(|_| Tx::parse(reader, testnet))
+            .collect();
+
+        Ok(Self {
+            header,
+            txs
+        })
+    }
+
+    pub fn validate_merkle_root(&self) -> bool {
+        let hashes = &self.txs.clone()
+            .into_iter()
+            .map(|tx| hex::decode(tx.id()).unwrap())
+            .collect::<Vec<Vec<u8>>>();
+
+        let computed_merkle_root: [u8; 32] = merkle_root(hashes.to_vec()).try_into().unwrap();
+
+        let expected = self.header.merkle_root;
+
+        expected == computed_merkle_root
+    }
+}
+
+impl Default for Block {
+    fn default() -> Self {
+        Self::new(BlockHeader::default(), vec![])
+    }
+}
--- programming-bitcoin/src/network/messages/block.rs
+++ programming-bitcoin/src/network/messages/block.rs
@@ -1,26 +1,20 @@
 use std::io::{Cursor, Error};
 
-use crate::{blocks::block_header::BlockHeader, network::network_message::NetworkMessage, transactions::tx::Tx, utils::varint::{encode_varint, read_varint}};
+use crate::{blocks::block::Block, network::network_message::NetworkMessage };
 
 #[derive(Clone)]
 pub struct BlockMessage {
     pub command: String,
-    pub header: BlockHeader,
-    pub tx_count: u64,
-    pub txs: Vec<Tx>
+    pub block: Block,
 }
 
 impl BlockMessage {
-    pub fn new(header: BlockHeader, txs: Vec<Tx>) -> Self {
+    pub fn new(block: Block) -> Self {
         let command = String::from("block");
-
-        let tx_count = txs.len() as u64;
 
         Self {
             command,
-            header,
-            tx_count,
-            txs,
+            block,
         }
     }
 }
@@ -34,14 +28,8 @@ impl NetworkMessage for BlockMessage {
     fn serialize(&self) -> Vec<u8> {
         let mut result = Vec::new();
 
-        let header = BlockHeader::serialize(&self.header);
-        result.extend_from_slice(&header);
-
-        result.extend_from_slice(&encode_varint(self.tx_count));
-
-        for tx in &self.txs {
-            result.extend_from_slice(&tx.serialize());
-        }
+        let block = &self.block.serialize();
+        result.extend_from_slice(block);
 
         result
     }
@@ -49,33 +37,21 @@ impl NetworkMessage for BlockMessage {
     fn parse(&self, reader: &mut Cursor<Vec<u8>>) -> Result<Self, Error> {
         let command = String::from("block");
 
-        let header = BlockHeader::parse(reader).unwrap();
-
-        let tx_count = read_varint(reader)?;
-
-        // set testnet as true for now
-        let testnet = true;
-
-        let mut txs = Vec::with_capacity(tx_count as usize);
-        for _ in 0..tx_count {
-            txs.push(Tx::parse(reader, testnet));
-        }
+        let block = Block::parse(reader).unwrap();
 
         Ok(Self {
             command,
-            header,
-            tx_count,
-            txs
+            block,
         })
     }
 
     async fn default_async(_: &str) -> Result<Self, Error> {
-        Ok(Self::new(BlockHeader::default(), vec![]))
+        Ok(Self::new(Block::default()))
     }
 }
 
 impl Default for BlockMessage {
     fn default() -> Self {
-        Self::new(BlockHeader::default(), vec![])
+        Self::new(Block::default())
     }
 }
